cf4123aee0d9f16b0c90066e47c31086ed342992
mateosanz <mateosanz55@Matthews-MacBook-Pro-5.local>
2014-04-24 09:45:00 -0400

    Updated BattleShip Server, Server now can accept two players

--- BattleServer.c
+++ BattleServer.c
@@ -4,7 +4,7 @@
    TCP SERVER CODE
    Designed for Linux OS
    Cited source: http://beej.us/guide/bgnet/output/html/singlepage/bgnet.html
- */
+*/
 
 /* Libraries To Include */
 #include <stdio.h>
@@ -17,24 +17,26 @@
 #include <arpa/inet.h>
 
 /* Definitions */
-#define PORT "3330" //port that is used for server and players
+#define PORT "3369" //port that is used for server and players
 #define MAX_PLAYERS 2 //max num of players
 
 int players[MAX_PLAYERS];
 
-/*Get Players*/
-
-int main(void) {
+int main(int argc, char *argv[]) {
 
 /* Variables To Include*/
   int sockfd; //socket file descriptor
   int status;
-  int player1, player2; //file descriptors for both players
+  struct sockaddr_storage player_addr;
+  int player; //file descriptors for both players (essentially newfd)
   int how; //use this to prevent a player from sending after his turn 
   struct addrinfo hints, *res;
   struct addrinfo *servinfo;
+  socklen_t addr_size;
+  int yes = 1;
+  int playersNeeded = 2;
 
-/*Establish Server*/
+/* Establish Server*/
   memset(&hints,0,sizeof(hints));
   hints.ai_family = AF_INET;
   hints.ai_socktype = SOCK_STREAM; //TCP socket type
@@ -42,9 +44,54 @@ int main(void) {
 
   if((status = getaddrinfo(NULL,PORT,&hints,&servinfo)) != 0) {
     fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
-    return(1); //Good Job You Failed :D
+    exit(1); //Good Job You Failed :D
   }
   
+  /* Wait to accept incoming connection*/
+  // loop through all results and bind to the first we can
 
+  for(res = servinfo; res != NULL; res = res->ai_next) {
+    if((sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1) {
+      perror("server: socket");
+      continue;
+    }
 
+    if(setsockopt(sockfd,SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+      perror("setsockopt");
+      exit(1);
+    }
+
+    if(bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
+      close(sockfd);
+      perror("server : bind");
+      continue;
+    }
+    break;
+  }  
+
+  if(res == NULL) {
+    fprintf(stderr, "server: failed to bind\n");
+    return 2; //Good Job You Failed :D
+  }
+
+  freeaddrinfo(servinfo); //all done with servinfo
+
+  if(listen(sockfd,MAX_PLAYERS) == -1) {
+    perror("listen");
+    exit(1);
+  }  
+  printf("Battleship Server : Waiting for %d Player(s) to Connect...\n",playersNeeded);
+
+  /*Get Players*/
+  while(playersNeeded != 0) {
+    addr_size = sizeof(player_addr);
+    player = accept(sockfd,(struct sockaddr *)&player_addr, &addr_size);
+    if (player == -1) {
+      perror("Error in Connecting Player to Server");
+      continue;
+    } else {
+      printf("Battleship Server : Waiting for %d Player(s) to Connect...\n",--playersNeeded);
+    }
+  }
+  printf("All players connected...BATTLE!!!\n");
 }
